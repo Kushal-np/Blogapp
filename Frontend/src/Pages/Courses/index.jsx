00a417151f126548a3e0c436f9251951e1dfdcfa
Kushal-np <wrckushal@gmail.com>
2025-07-21 17:55:07 +0545

    god please

--- Frontend/src/Pages/Courses/index.jsx
+++ Frontend/src/Pages/Courses/index.jsx
@@ -1,23 +1,16 @@
-import { useState , useEffect } from 'react';
-import Card from '../../Components/CardComponent';
-import Footer from '../../Components/Footer';
-import Header from '../../Components/Header';
-import Navbar from '../../Components/Navbar';
-import axios from 'axios'
-import { AddBook } from '../../../../Backend/controllers/book.controller';
-import AddBooks from '../../Components/AddBooks';
 function Courses() {
   const [book, setBook] = useState([]);
 
+  const getBook = async () => {
+    try {
+      const res = await axios.get("https://bookish-767c.onrender.com/api/v1/book/getBook");
+      setBook(res.data.book);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   useEffect(() => {
-    const getBook = async () => {
-      try {
-        const res = await axios.get("https://bookish-767c.onrender.com/api/v1/book/getBook");
-        setBook(res.data.book);
-      } catch (error) {
-        console.log(error);
-      }
-    };
     getBook();
   }, []);
 
@@ -25,9 +18,8 @@ function Courses() {
     <div>
       <Navbar />
       <Header />
-      <AddBooks />
+      <AddBooks onBookAdded={getBook} /> {/* <-- FIX here */}
 
-      {/* Cards container wrapper for max width and center */}
       <div className="bg-black py-12">
         <div className="max-w-[1200px] mx-auto px-4">
           <div className="flex flex-wrap gap-8 justify-center lg:grid lg:grid-cols-3 lg:grid-rows-auto">
@@ -49,5 +41,3 @@ function Courses() {
     </div>
   );
 }
-
-export default Courses;
